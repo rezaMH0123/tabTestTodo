0ccc60e1b021a833c8c610d7e4190f7368777178
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-01-31 17:41:41 +0330

    fixd height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 
   return (
     <div className="h-screen bg-white flex  justify-center md:items-center  relative">
-      <div className="bg-zinc-700 p-4 containerTodo flex flex-col items-center md:rounded-xl shadow-3xl w-full  sm:w-[80%] md:w-[75%] lg:w-[55%]">
+      <div className="bg-zinc-700 p-4 containerTodo flex flex-col items-center md:rounded-xl shadow-3xl h-screen sm:h-[600px] w-full  sm:w-[80%] md:w-[75%] lg:w-[55%]">
         <h2 className="t mt-4 mb-4 text-[#cc641f] font-bold text-lg">
           TODO APP
         </h2>
